86ee11d152ccb6aea763dc0a0c7598f4b7bc4d0c
cherukurirajesh <rajeshcherukuri83@gmail.com>
2017-08-16 13:50:30 +0530

    Adding first project summary
    Adding first project

---
+++ LearningVisualStudio/hackerrank/hackerrank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hackerrank
+{
+    class Day00helloworld
+    {
+        static void Main(string[] args)
+        {
+            string inputstring;
+            inputstring = Console.ReadLine();
+            Console.WriteLine("Hello World");
+            Console.WriteLine(inputstring);
+            Console.ReadKey();
+        }
+    }
+}

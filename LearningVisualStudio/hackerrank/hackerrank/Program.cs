0ce24eab61f85140a95481642d87d1b136dab937
cherukurirajesh <rajeshcherukuri83@gmail.com>
2017-08-17 14:46:32 +0530

    Day01 Program done

--- LearningVisualStudio/hackerrank/hackerrank/Program.cs
+++ LearningVisualStudio/hackerrank/hackerrank/Program.cs
@@ -10,33 +10,39 @@ namespace hackerrank
     {
         static void Main(string[] args)
         {
-            int i01 = 4;
-            string s01 = "HackerRank", inputstring;
-            Double d01 = 0.4;
-            inputstring = Console.ReadLine();
+            String inputstring = Console.ReadLine();
             Console.WriteLine("Hello World");
             Console.WriteLine(inputstring);
             Console.ReadKey();
-
             Day01DataTypes DT = new Day01DataTypes();
-            DT.Day01DataTypes();
+            DT.DataTypesMethod();
+            Console.ReadKey();
         }
     }
 
     class Day01DataTypes 
     {
+        int i01 = 4;
+        string S01 = "HackerRank", inputstringD01, CompleteString;
+        Double d01 = 4.0;
         int i, SumofIntegers;
         Double d, SumofDouble;
-        String s, inputstring, CompleteString;
-        Console.WriteLine("Please enter the Integer");
-        i = Console.ReadLine();
-        SumofIntegers = (i01+i);
-        Console.WriteLine(SumofIntegers);
-        Console.WriteLine("Please enter the double number");
-        d = Console.ReadLine();
-        SumofDouble = (d+d01);
-        Console.WriteLine("Please enter the String");
-        inputstring = Console.ReadLine();
-        CompleteString = string.Concat(S01, inputstring);
+
+        public void DataTypesMethod()
+        {
+            Console.WriteLine("Please enter the Integer");
+            i = int.Parse(Console.ReadLine());
+            SumofIntegers = (i01 + i);
+            Console.WriteLine(SumofIntegers);
+            Console.WriteLine("Please enter the double number");
+            d = double.Parse(Console.ReadLine());
+            SumofDouble = ((d + d01));
+            //Console.WriteLine(SumofDouble);
+            Console.WriteLine((d + d01).ToString("F1"));
+            Console.WriteLine("Please enter the String");
+            inputstringD01 = Console.ReadLine();
+            CompleteString = string.Concat(S01 + " " + inputstringD01);
+            Console.WriteLine(CompleteString);
+        }
     }
 }

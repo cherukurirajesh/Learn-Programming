9fd836f393123019ddb21ea9cb6c6ab0b37b6617
cherukurirajesh <rajeshcherukuri83@gmail.com>
2017-08-17 09:17:01 +0530

    Day01 commits

--- LearningVisualStudio/hackerrank/hackerrank/Program.cs
+++ LearningVisualStudio/hackerrank/hackerrank/Program.cs
@@ -10,11 +10,33 @@ namespace hackerrank
     {
         static void Main(string[] args)
         {
-            string inputstring;
+            int i01 = 4;
+            string s01 = "HackerRank", inputstring;
+            Double d01 = 0.4;
             inputstring = Console.ReadLine();
             Console.WriteLine("Hello World");
             Console.WriteLine(inputstring);
             Console.ReadKey();
+
+            Day01DataTypes DT = new Day01DataTypes();
+            DT.Day01DataTypes();
         }
+    }
+
+    class Day01DataTypes 
+    {
+        int i, SumofIntegers;
+        Double d, SumofDouble;
+        String s, inputstring, CompleteString;
+        Console.WriteLine("Please enter the Integer");
+        i = Console.ReadLine();
+        SumofIntegers = (i01+i);
+        Console.WriteLine(SumofIntegers);
+        Console.WriteLine("Please enter the double number");
+        d = Console.ReadLine();
+        SumofDouble = (d+d01);
+        Console.WriteLine("Please enter the String");
+        inputstring = Console.ReadLine();
+        CompleteString = string.Concat(S01, inputstring);
     }
 }
